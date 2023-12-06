bc5e4519f39c2685037dd961013b1beddc4d0055
Sayeed Rizwan <sayeed.r.99@gmail.com>
2023-12-06 17:59:44 +0530

    intial commit

---
+++ src/test/java/com/basirhat/resultservice/ResultserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.basirhat.resultservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResultserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
